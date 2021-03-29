c839b66b94ac3971f1deef1d42d66b0f0268bded
Brian Pulfer <42559439+BrianPulfer@users.noreply.github.com>
2021-03-29 18:35:14 +0200

    Saving best model only (XTE predictor).

--- thesis/cyclegan/training/train_xte_predictor.py
+++ thesis/cyclegan/training/train_xte_predictor.py
@@ -110,7 +110,7 @@ def main():
               validation_data=(x_val, y_val),
               callbacks=[
                   EarlyStopping('val_loss', patience=10),
-                  ModelCheckpoint('../xte_predictor.h5', 'val_loss', save_weights_only=False)
+                  ModelCheckpoint('../xte_predictor.h5py', 'val_loss', save_best_only=True, save_weights_only=False)
               ]
               ).history
 
