6876ce71aff4e933b9f5a651026cc9200993fdda
Brian Pulfer <42559439+BrianPulfer@users.noreply.github.com>
2021-06-04 15:46:13 +0200

    Added code to scroll through images of the alligned tubs.

--- thesis/xte_predictor/cyclegan_quality_test/compare_images.py
+++ thesis/xte_predictor/cyclegan_quality_test/compare_images.py
@@ -26,3 +26,36 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def scroll_through_images():
+    import cv2
+
+    base = os.path.join(pathlib.Path(__file__).parent.absolute(), "alligned_sim_real_tub")
+    ri_path, pri_path = os.path.join(base, "real"), os.path.join(base, "pseudoreal")
+    si_path, psi_path = os.path.join(base, "sim"), os.path.join(base, "pseudosim")
+
+    ri_names = sorted(list(os.listdir(ri_path)), key=lambda name: int(name.split("_")[0]))
+    pri_names = sorted(list(os.listdir(pri_path)), key=lambda name: int(name.split("_")[1]))
+    si_names = sorted(list(os.listdir(si_path)), key=lambda name: int(name.split("_")[1].split(".")[0]))
+    psi_names = sorted(list(os.listdir(psi_path)), key=lambda name: int(name.split("_")[0]))
+
+    for r, s, pr, ps in zip(ri_names, si_names, pri_names, psi_names):
+        real = cv2.resize(cv2.imread(os.path.join(ri_path, r)), (256, 256))
+        sim = cv2.resize(cv2.imread(os.path.join(si_path, s)), (256, 256))
+        preal = cv2.imread(os.path.join(pri_path, pr))
+        psim = cv2.imread(os.path.join(psi_path, ps))
+
+        cv2.imshow("real", real)
+        cv2.imshow("sim", sim)
+        cv2.imshow("pseudo-real", preal)
+        cv2.imshow("pseudo-sim", psim)
+
+        key = cv2.waitKey()
+
+        # Press 'S' to store images
+        if key == 115:
+            cv2.imwrite(os.path.join(base, "real.jpg"), real)
+            cv2.imwrite(os.path.join(base, "sim.jpg"), sim)
+            cv2.imwrite(os.path.join(base, "preal.png"), preal)
+            cv2.imwrite(os.path.join(base, "psim.png"), psim)
